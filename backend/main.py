0117ca8bb3b4c7ba96b84ba9ca26240bd4a828c2
Work Email <shedrack.aji@outlook.com>
2024-08-12 10:57:23 +0100

    first commit

--- backend/main.py
+++ backend/main.py
@@ -27,4 +27,6 @@ async def get_prediction(weather_variable: str = Form(...), days: int = Form(...
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
     # use with render
-    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+  
+
